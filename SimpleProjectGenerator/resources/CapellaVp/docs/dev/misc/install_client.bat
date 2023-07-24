a86488343a76903bf7c5ed245e101728a7094280
Nicolas PERANSIN <nicolas.peransin@obeo.fr>
2023-07-24 10:50:36 +0200

    Add batch for Capella product testing.

---
+++ SimpleProjectGenerator/resources/CapellaVp/docs/dev/misc/install_client.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set CAPELLA_HOME=C:\dev\vv\capella-6.1\capella
+set ECLIPSE_EXE=%CAPELLA_HOME%\capellac.exe
+
+REM Path for a folder
+set P2_NAME=%inject:project.fullname%.site
+
+set P2_LOCATION=file:///%~dp0../../../releng/%P2_NAME%/target/repository/
+REM Path for an archive
+REM set P2_LOCATION=jar:file:///%~dp0../../../releng/%P2_NAME%/target/%P2_NAME%-1.0.0-SNAPSHOT.zip!/
+
+REM Syntax for several P2 locations
+set ECLIPSE_P2=https://download.eclipse.org/releases/2021-06
+set P2_SET=%P2_LOCATION%,%ECLIPSE_P2%
+set P2_SET=%P2_LOCATION%
+
+set EXTENSION_FEATURES=%inject:project.fullname%.feature.feature.group
+
+set WS=%~dp0..\..\..\releng\%P2_NAME%\target\ws_install
+
+"%ECLIPSE_EXE%" ^
+ -clean -noSplash -console -consoleLog ^
+ -data %WS% ^
+ -application org.eclipse.equinox.p2.director ^
+ -purgeHistory ^
+ -profile DefaultProfile ^
+ -tag InstallOAx ^
+ -repository %P2_SET%  ^
+ -installIUs %EXTENSION_FEATURES%
+
