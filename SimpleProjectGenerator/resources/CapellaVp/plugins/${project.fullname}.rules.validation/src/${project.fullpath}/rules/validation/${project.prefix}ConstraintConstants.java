b8c243ff060b9e5b6988cca3376d6f5e6c0b5e63
Nicolas PERANSIN <nicolas.peransin@obeo.fr>
2023-07-26 09:18:18 +0200

    EMF/Capella templates.

---
+++ SimpleProjectGenerator/resources/CapellaVp/plugins/${project.fullname}.rules.validation/src/${project.fullpath}/rules/validation/${project.prefix}ConstraintConstants.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * %inject:project.copyright%
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package %inject:project.fullname%.rules.validation;
+
+/**
+ * Constants for Constraints.
+ * 
+ * @author %inject:user.name%
+ */
+public class %inject:project.prefix%ConstraintConstants {
+
+    /** Plugin id */
+    public static final String PLUGIN_ID =
+        "%inject:project.fullname%.rules.validation"; //$NON-NLS-1$
+
+    /** Category id */
+    public static final String CATEGORY =
+        "%inject:validation.category%"; //$NON-NLS-1$
+
+}
--- SimpleProjectGenerator/resources/emf/plugins/${project.fullname}.design/src/${project.fullpath}/design/${project.prefix}ModelEdits.java
+++ SimpleProjectGenerator/resources/emf/plugins/${project.fullname}.design/src/${project.fullpath}/design/${project.prefix}ModelEdits.java
@@ -17,11 +17,15 @@ public class %inject:project.prefix%ModelEdits {
 
     /**
      * Logs a AQL call in IO/Standard stream.
+     * <p>
+     * Should only be used during development time.
+     * </p>
      * 
      * @param it debugged element
      * @param message to print
      * @return provided element
      */
+    @Deprecated
     public static final EObject to%inject:project.prefix%Debug(EObject it, String message) {
         System.out.println("VSM-Call " + message + ": " + it);  //$NON-NLS-1$ //$NON-NLS-2$
         return it;
